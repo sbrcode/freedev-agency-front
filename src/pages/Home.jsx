9704949b4ee6120e446883d3d47b0632bda9fbb9
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2022-04-21 15:26:26 +0200

    prettier

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +9,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Home
+
---
+++ src/pages/Survey.jsx
@@ -0,0 +1,9 @@
+const Survey = () => {
+  return (
+    <div>
+      <h1>Questionnaire 🧮</h1>
+    </div>
+  )
+}
+
+export default Survey
