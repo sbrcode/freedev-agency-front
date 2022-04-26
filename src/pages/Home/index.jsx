1143652f6111d15bca204a2a8e97e7aa12888c97
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2022-04-26 16:36:45 +0200

    change-home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,9 +2,7 @@ const Home = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is Home</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
