ececaefc8b4aeba972ca93bf137a84561145df94
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2022-05-03 17:51:26 +0200

    result-page

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -1,7 +1,15 @@
+import { useContext } from 'react'
+import { SurveyContext } from '../../utils/context'
+
 const Results = () => {
+  const { answers } = useContext(SurveyContext)
+
   return (
     <div>
       <h1>Results</h1>
+      <ol>
+        {answers !== {} && Object.values(answers).map((answer, index) => <li key={index}>{answer.toString()}</li>)}
+      </ol>
     </div>
   )
 }
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
+import { SurveyContext } from '../../utils/context'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import colors from '../../utils/style/colors'
 import { Loader } from '../../utils/style/Atoms'
+import colors from '../../utils/style/colors'
 
 const SurveyContainer = styled.div`
   display: flex;
@@ -26,6 +27,30 @@ const LinkWrapper = styled.div`
   }
 `
 
+const ReplyBox = styled.button`
+  border: none;
+  height: 100px;
+  width: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.backgroundLight};
+  border-radius: 30px;
+  cursor: pointer;
+  box-shadow: ${(props) => (props.isSelected ? `0px 0px 0px 2px ${colors.primary} inset` : 'none')};
+  &:first-child {
+    margin-right: 15px;
+  }
+  &:last-of-type {
+    margin-left: 15px;
+  }
+`
+
+const ReplyWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
 const Survey = () => {
   const params = useParams()
   const questionNumber = parseInt(params.questionId)
@@ -33,6 +58,7 @@ const Survey = () => {
   const nextQuestionNumber = questionNumber + 1
   const [surveyData, setSurveyData] = useState({})
   const [isDataLoading, setDataLoading] = useState(false)
+  const { answers, saveAnswers } = useContext(SurveyContext)
 
   useEffect(() => {
     fetchData()
@@ -50,10 +76,22 @@ const Survey = () => {
     }
   }
 
+  const saveReply = (answer) => {
+    saveAnswers({ [questionNumber]: answer })
+  }
+
   return (
     <SurveyContainer>
       <QuestionTitle>Question {questionNumber}</QuestionTitle>
       {isDataLoading ? <Loader /> : <QuestionContent>{surveyData[questionNumber]} </QuestionContent>}
+      <ReplyWrapper>
+        <ReplyBox onClick={() => saveReply(true)} isSelected={answers[questionNumber] === true}>
+          Oui
+        </ReplyBox>
+        <ReplyBox onClick={() => saveReply(false)} isSelected={answers[questionNumber] === false}>
+          Non
+        </ReplyBox>
+      </ReplyWrapper>
       <LinkWrapper>
         <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
         {surveyData[questionNumber + 1] ? (
